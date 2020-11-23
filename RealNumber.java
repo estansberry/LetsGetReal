af618a1df7d28ae5a0d0dde5af882cb4eec508ad
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-11-23 09:36:49 -0700

    lookin good

---
+++ RealNumber.java
@@ -0,0 +1,49 @@
+public class RealNumber{
+  public static void main(String args[]){
+    
+  }
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return "" + getValue();
+  }
+
+  public boolean equals(RealNumber other){
+    if(getValue() == 0 && other.getValue() == 0){
+      return true;
+    }else if(getValue() == 0 || other.getValue() == 0){
+      return false;
+    }if(getValue() / other.getValue() <= 0.00001 || other.getValue() / getValue() <= 0.00001){
+      return true;
+    }
+    return false;
+  }
+
+  public RealNumber add(RealNumber other){
+    RealNumber newval = new RealNumber(getValue() + other.getValue());
+     return (newval);
+  }
+
+  public RealNumber multiply(RealNumber other){
+    RealNumber newval = new RealNumber(getValue() * other.getValue());
+      return(newval);
+  }
+
+  public RealNumber divide(RealNumber other){
+    RealNumber newval = new RealNumber(getValue() / other.getValue());
+      return(newval);
+  }
+
+  public RealNumber subtract(RealNumber other){
+    RealNumber newval = new RealNumber(getValue() - other.getValue());
+    return (newval);
+  }
+}
